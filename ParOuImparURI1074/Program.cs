3b2d8acccee142555515b6e22aa203afbaf6edd2
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-06-16 17:28:29 -0300

    Add project files.

---
+++ ParOuImparURI1074/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ParOuImparURI1074
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <=n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                if (x % 2==0 && x > 0)
+                {
+                    Console.WriteLine("EVEN POSITIVE");
+                }
+
+                if (x % 2 != 0 && x > 0)
+                {
+                    Console.WriteLine("ODD POSITIVE");
+                }
+
+                if (x % 2 == 0 && x < 0)
+                {
+                    Console.WriteLine("EVEN NEGATIVE");
+                }
+
+                if (x % 2 != 0 && x < 0)
+                {
+                    Console.WriteLine("ODD NEGATIVE");
+                }
+
+                if (x == 0)
+                {
+                    Console.WriteLine("NULL");
+                }
+            }
+        }
+    }
+}
